eee216c6d77d782a7d3d1a5a4f654b9b8439a360
Shane Dell <shanedell100@gmail.com>
2024-06-26 10:12:20 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Azure Runbooks
+
+This repository contains different Runbooks for use in Azure.
+
+- `vms` - Contains runbooks used for VM operations
---
+++ vms/start-workflow.ps1
@@ -0,0 +1,24 @@
+workflow startVM {
+    param(
+        [string]$vmName,
+        [string]$resourceGroup
+    )
+    InlineScript {
+        # Ensures you do not inherit an AzContext in your runbook
+        $null = Disable-AzContextAutosave -Scope Process
+
+        # Connect using a Managed Service Identity
+        try {
+            $AzureConnection = (Connect-AzAccount -Identity).context
+        }
+        catch {
+            Write-Output "There is no system-assigned user identity. Aborting." 
+            exit
+        }
+
+        # set and store context
+        $AzureContext = Set-AzContext -SubscriptionName $AzureConnection.Subscription -DefaultProfile $AzureConnection
+
+        Start-AzVM -ResourceGroupName $using:resourceGroup -Name $using:vmName
+    }
+}
